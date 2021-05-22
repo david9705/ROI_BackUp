c90d51169b383f6642b031ed6dd813736872d6d3
david9705 <abc88662a@gmail.com>
2021-05-22 13:57:22 +0000

    0522__ClearSetting

---
+++ ClearAll.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearAll : MonoBehaviour
+{
+
+
+    public bool clear = false;
+    public GameObject PM;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(clear)
+        {
+            foreach(Transform child in PM.transform)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+        
+    }
+}
