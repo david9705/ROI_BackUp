5604ded1ef897faed706d130c204d90bef6b07e2
david9705 <abc88662a@gmail.com>
2021-05-27 19:36:44 +0000

    0528_ButtonDisplay

---
+++ ButtonDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonDisplay : MonoBehaviour
+{
+
+    [SerializeField]
+    private TextMesh ButtonText;    
+
+    public GameObject PlaceButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ButtonText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ButtonInteraction.PosState == PosButtonType.PLACE)
+        {
+            ButtonText.text = "Place";
+        }
+        else if(ButtonInteraction.PosState == PosButtonType.DELETE)
+        {
+            ButtonText.text = "Delete";
+        }
+        else if(ButtonInteraction.PosState == PosButtonType.CANCEL)
+        {
+            ButtonText.text = "Edit";
+        }
+        
+    }
+}
